4cff999815533098578d0c7807a84ffa29e79cc7
Iliass Badou <97060220+FIGOSHIIN@users.noreply.github.com>
2025-05-26 12:27:54 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "YOU API KEY",
+    apiKey: "YOUR API KEY",
     authDomain: "cooking-figo.firebaseapp.com",
     projectId: "cooking-figo",
     storageBucket: "cooking-figo.firebasestorage.app",
